d4613418600b6f23e5e334c2273ce64b6c9aa649
Renan Vieira <renanvieira.webdesign@gmail.com>
2023-01-26 14:04:42 -0300

    [feature] added style to nav-bar.

--- src/constans.tsx
+++ src/constans.tsx
@@ -1,3 +1,4 @@
+import { HomeOutlined as HomeIcon, Paid } from "@mui/icons-material";
 import Home from "features/Home/Home";
 import OperationList from "features/OperationList/OperationList";
 import { RouterConfig } from "models/routes";
@@ -5,11 +6,13 @@ import { RouterConfig } from "models/routes";
 export const routes: RouterConfig[] = [
   {
     routeName: "Página inicial",
+    icon: <HomeIcon />,
     path: "/",
     element: <Home />,
   },
   {
     routeName: "Lista de transações",
+    icon: <Paid />,
     path: "/operation-list",
     element: <OperationList />,
   },
--- src/models/routes.ts
+++ src/models/routes.ts
@@ -2,4 +2,5 @@ import { RouteObject } from "react-router-dom";
 
 export type RouterConfig = {
   routeName: string;
+  icon?: JSX.Element;
 } & RouteObject;
